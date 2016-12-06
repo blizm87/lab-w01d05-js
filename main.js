ed43e71c4d467b0dbd674d00458674203dc9d474
Justin Samuels <blizm87@gmail.com>
2016-12-06 10:45:15 -0800

    Fully functional To do list app.

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 var remListItem = function(){
   this.parentNode.remove();
+  taskTotal = document.querySelectorAll('li').length;
+  document.querySelector('#counter').textContent= 'Completed ' + tasksCompleted + ' out of ' + taskTotal;
+
 }
 
 var remComplete = function (){
   this.removeAttribute('class','complete');
+  tasksCompleted = document.querySelectorAll('.complete').length;
+  document.querySelector('#counter').textContent= 'Completed ' + tasksCompleted + ' out of ' + taskTotal;
   this.addEventListener('click',remListItem);
+
 }
 
 var addComplete = function (){
   this.setAttribute('class','complete');
-  this.addEventListener('click', remComplete);
+  tasksCompleted = document.querySelectorAll('.complete').length;
+  document.querySelector('#counter').textContent= 'Completed ' + tasksCompleted + ' out of ' + taskTotal;
+  this.addEventListener('click',remComplete);
 }
 
 var renderText = function () {
@@ -22,16 +30,24 @@ var renderText = function () {
   if (event.keyCode === 13){
     newLiItem.setAttribute('class','todo');
     inputTextItem.addEventListener('click', addComplete);
-    xOutItem.addEventListener('click',remListItem);
     inputTextItem.innerHTML = getInput;
     xOutItem.innerHTML = ' x';
     textList.appendChild(newLiItem);
     newLiItem.appendChild(inputTextItem);
     newLiItem.appendChild(xOutItem);
+    taskTotal = document.querySelectorAll('li').length;
+    xOutItem.addEventListener('click',remListItem);
+    document.querySelector('#counter').textContent= 'Completed ' + tasksCompleted + ' out of ' + taskTotal;
     event.target.value=" ";
+
   }
 }
 
+var tasksCompleted = 0;
+var taskTotal = 0;
+document.querySelector('#counter').textContent= 'Completed ' + tasksCompleted + ' out of ' + taskTotal;
+
 var textBox1 = document.querySelector('input.new-todo');
 textBox1.addEventListener('keyup', renderText);
+
 
